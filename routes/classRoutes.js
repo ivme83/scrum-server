07966e4944b0da14339f2be4c63ac18fad4b07c4
Roland Willis <rolandwillisiv@gmail.com>
2018-05-28 22:00:06 -0700

    Deploy to heroku

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -22,9 +22,9 @@ router.post('/', (req, res) => {
     db.Class.create(newClassData, (err, newClass) => {
         if (err) return next(err);
 
-        db.User.findOne({'fbuid': req.body.teacher_id}, (err, foundUser) => {
+        db.User.findOne({ 'fbuid': req.body.teacher_id }, (err, foundUser) => {
 
-            db.Teacher.findOneAndUpdate({user_id: foundUser._id}, { $push: { classes: newClass._id } }, { new: true }, (err, updated) => {
+            db.Teacher.findOneAndUpdate({ user_id: foundUser._id }, { $push: { classes: newClass._id } }, { new: true }, (err, updated) => {
                 if (err) return next(err);
                 console.log(updated);
                 res.json(newClass);
@@ -36,20 +36,48 @@ router.post('/', (req, res) => {
 });
 
 router.post("/getClasses", (req, res) => {
-  let fbuid = req.body.fbuid;
+    let fbuid = req.body.fbuid;
 
-  db.User.findOne({ fbuid: fbuid }, (err, foundUser) => {
-    db.Teacher.findOne({ user_id: foundUser._id })
-      .populate("classes")
-      .then(userClasses => {
-          console.log(userClasses);
-        res.json(userClasses);
-      })
-      .catch(function(err) {
-        // If an error occurs, send it back to the client
-        res.json(err);
-      });
-  });
+    db.User.findOne({ fbuid: fbuid }, (err, foundUser) => {
+        if (foundUser.role === "teacher") {
+            db.Teacher.findOne({ user_id: foundUser._id })
+                .populate("classes")
+                .then(userClasses => {
+                    console.log(userClasses);
+                    res.json(userClasses);
+                })
+                .catch(function (err) {
+                    // If an error occurs, send it back to the client
+                    res.json(err);
+                });
+        } else {
+            db.Student.findOne({ user_id: foundUser._id })
+                .populate("classes")
+                .then(userClasses => {
+                    console.log(userClasses);
+                    res.json(userClasses);
+                })
+                .catch(function (err) {
+                    // If an error occurs, send it back to the client
+                    res.json(err);
+                });
+
+        });
+});
+
+router.post("/addClass", (req, res) => {
+    let fbuid = req.body.fbuid;
+    let class_code = req.body.class_code;
+
+    db.Class.findOne({class_code: class_code}, (err, foundClass) => {
+        db.User.findOne({ fbuid: fbuid }, (err, foundUser) => {
+            db.Student.findOneAndUpdate({ user_id: foundUser._id }, { $push: { classes: foundClass._id } }, { new: true }, (err, updated) => {
+                if (err) return next(err);
+                console.log(updated);
+                res.json(newClass);
+            });
+        });
+    });
 });
 
 module.exports = router;
